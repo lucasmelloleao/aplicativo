4abd90a1e4c7f4f7d290e2c33c70602a88bb2e3c
Lucas Leao <lucasmelloleao@gmail.com>
2021-03-16 17:17:06 +0000

    Primeiro commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :members
+  devise_for :admins
+  get 'home/index'
+  root 'home#index'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
